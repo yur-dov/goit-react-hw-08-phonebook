0bba6a9fa4e5646d7e405020c59cb0bd82ea4656
yur-dov <89393975+yur-dov@users.noreply.github.com>
2022-09-08 18:16:51 +0000

    Deploying to gh-pages from @ yur-dov/goit-react-hw-08-phonebook@2a3caa320f8b2ace3cf69587ccfe392ff9bc075e 🚀

---
+++ static/js/home-page.c657717a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[223],{6561:function(e,t,a){a.r(t);var r=a(8909),s=a(4959),c=a(184);t.default=function(){return(0,c.jsxs)(r.Z,{children:[(0,c.jsx)("h1",{children:"Save your contacts with our solution"}),(0,c.jsx)("img",{src:s,alt:"User avatar"})]})}},4959:function(e,t,a){e.exports=a.p+"static/media/image.38ff745269195f7985ae.jpg"}}]);
+//# sourceMappingURL=home-page.c657717a.chunk.js.map
